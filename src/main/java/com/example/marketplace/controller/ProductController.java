532c0a7fc226d7874eeb0546e5a92248018abd21
Andy <yanandy1804@gmail.com>
2026-02-23 21:43:49 +0000

    added product detail functionality

--- src/main/java/com/example/marketplace/controller/ProductController.java
+++ src/main/java/com/example/marketplace/controller/ProductController.java
@@ -1,11 +1,23 @@
 package com.example.marketplace.controller;
+import com.example.marketplace.model.Product;
+import com.example.marketplace.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
-    @GetMapping("/product/detail")
-    public String product() {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/detail/{id}")
+    public String productDetail(@PathVariable Integer id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
         return "product-detail";
     }
 }
